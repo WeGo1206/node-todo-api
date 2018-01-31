5934b10d3474333cf48c3d37b875657ff1229c20
WeGo1206 <gweissflog@hotmail.com>
2018-01-31 18:28:44 +0100

    delete route

--- server/server.js
+++ server/server.js
@@ -50,6 +50,24 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+    console.log('par:', id);
+    if(!ObjectID.isValid(id)) {
+      console.log('Id not valid', id);
+      return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.status(200).send({todo});
+    }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
